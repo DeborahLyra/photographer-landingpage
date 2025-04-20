ddfc47b0d81c0cf897fb9693a88211d421236840
Deborah Lyra <deborahp.lyra@gmail.com>
2025-04-20 10:39:56 -0300

    added: image banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,15 @@
 import { motion } from "framer-motion";
 import image from '../../public/imgs/bannerImg.jpeg'
 
+interface ImageEvent extends React.MouseEvent<HTMLImageElement> {
+    preventDefault: () => void;
+}
+
 export function Banner() {
+    const handleContextMenu = (e: ImageEvent) => {
+        e.preventDefault(); 
+    };
+
     return (
         <>
             <div className="w-full flex justify-center items-center h-32 md:h-64">
@@ -14,11 +22,18 @@ export function Banner() {
                     MARCUS PRADO
                 </motion.h1>
             </div>
-            <div>
-                <img src={image} 
+
+            <div className="w-full relative">
+                <img 
+                src={image} 
                 alt="cidade de olinda e idoso" 
                 className="w-full h-[550px]"
-                />
+                onContextMenu={handleContextMenu} 
+                 />
+
+                <div className="absolute bottom-2 right-2 text-white text-sm opacity-50 pointer-events-none select-none">
+                    © Marcus Prado
+                </div>
             </div>
         </>
     );
